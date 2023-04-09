9e874dd586746bea9ce06f9a66163892cd889211
Александр <discegaudere113399@gmail.com>
2023-04-10 02:50:49 +0300

    second commit

--- S7/src/main/java/org/example/ViewCalculator.java
+++ S7/src/main/java/org/example/ViewCalculator.java
@@ -1,11 +1,12 @@
 package org.example;
+
 import javax.imageio.IIOException;
 import java.io.IOException;
 import java.util.logging.*;
 import java.util.Scanner;
 
 
-public class ViewCalculator implements IViewCalculator{
+public class ViewCalculator implements IViewCalculator {
 
     private CalculatorFactory calculatorFactory;
     private Calculator calculator;
@@ -27,11 +28,11 @@ public class ViewCalculator implements IViewCalculator{
             double imag1 = scanner.nextDouble();
             calculator = (Calculator) calculatorFactory.create(real1, imag1);
 //            logger.log(Level.INFO, "Калькулятор успешно принял первое комплексное число");
-        } catch (Exception e){
+            System.out.println("Logger: Калькулятор успешно принял первое комплексное число");
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
         while (true) {
-            try {
                 System.out.println("Введите команду (*, +, =) : ");
                 String cmd = scanner.next();
                 if (cmd.equals("*")) {
@@ -41,6 +42,7 @@ public class ViewCalculator implements IViewCalculator{
                     double imag2 = scanner.nextDouble();
                     calculator = calculator.mult((Calculator) calculatorFactory.create(real2, imag2));
 //                    logger.log(Level.INFO, "Калькулятор произвел умножение");
+                    System.out.println("Logger: Калькулятор успешно произвел умножение");
                 } else if (cmd.equals("/")) {
                     System.out.println(("Введите реальную часть второго комплексного числа: "));
                     double real2 = scanner.nextDouble();
@@ -48,6 +50,7 @@ public class ViewCalculator implements IViewCalculator{
                     double imag2 = scanner.nextDouble();
                     calculator = calculator.divide((Calculator) calculatorFactory.create(real2, imag2));
 //                    logger.log(Level.INFO, "Калькулятор произвел деление");
+                    System.out.println("Logger: Калькулятор успешно произвел деление");
                 } else if (cmd.equals("+")) {
                     System.out.println(("Введите реальную часть второго комплексного числа: "));
                     double real2 = scanner.nextDouble();
@@ -55,6 +58,7 @@ public class ViewCalculator implements IViewCalculator{
                     double imag2 = scanner.nextDouble();
                     calculator = calculator.plus((Calculator) calculatorFactory.create(real2, imag2));
 //                    logger.log(Level.INFO, "Калькулятор произвел сложение");
+                    System.out.println("Logger: Калькулятор успешно произвел сложение");
                 } else if (cmd.equals("-")) {
                     System.out.println(("Введите реальную часть второго комплексного числа: "));
                     double real2 = scanner.nextDouble();
@@ -62,24 +66,25 @@ public class ViewCalculator implements IViewCalculator{
                     double imag2 = scanner.nextDouble();
                     calculator = calculator.sub((Calculator) calculatorFactory.create(real2, imag2));
 //                    logger.log(Level.INFO, "Калькулятор произвел вычитание");
+                    System.out.println("Logger: Калькулятор успешно произвел вычитание");
                 } else if (cmd.equals("=")) {
                     String result = calculator.toString();
                     System.out.println("Результат: " + result);
 //                    logger.log(Level.INFO, "Калькулятор вывел результат");
                     System.out.println("Хотите посчитать еще раз? (Y - да, любая другая клавиша - нет)");
                     cmd = scanner.next();
-                    if (cmd.toLowerCase().equals("n")){
+                    if (cmd.toLowerCase().equals("n")) {
 //                        logger.log(Level.INFO, "Калькулятор завершил работу");
+                        System.out.println("Logger: Калькулятор завершил работу");
                         break;
                     }
                 }
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
+//            } catch (Exception e) {
+//                System.out.println(e.getMessage());
+//            }
         }
         try {
-        }
-        finally{
+        } finally {
             scanner.close();
         }
     }
